46518a2ad7845179773e38a2b619fe6ff31c3071
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-01-10 18:09:25 -0700

    render

---
+++ render-utils.js
@@ -0,0 +1,9 @@
+export function renderBunny(bunny) {
+    const bunnyEl = document.createElement('div');
+
+    bunnyEl.classList.add('bunny');
+    bunnyEl.textContent = bunny.name;
+
+    bunnyEl.append(bunny);
+    return bunnyEl;
+}
